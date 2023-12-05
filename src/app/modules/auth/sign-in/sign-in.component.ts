a9362c67a42d7f1a094a132584cab9e9acb1febd
Zikri89 <asalngoding89@gmail.com>
2023-12-05 10:55:11 +0700

    minor

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -82,9 +82,8 @@ export class AuthSignInComponent implements OnInit
 
         // Sign in
         this._authService.signIn(this.signInForm.value)
-            .subscribe(
-                () =>
-                {
+            .subscribe({
+                next: () => {
                     // Set the redirect url.
                     // The '/signed-in-redirect' is a dummy url to catch the request and redirect the user
                     // to the correct page after a successful sign in. This way, that url can be set via
@@ -93,10 +92,8 @@ export class AuthSignInComponent implements OnInit
 
                     // Navigate to the redirect url
                     this._router.navigateByUrl(redirectURL);
-
                 },
-                (response) =>
-                {
+                error: (response) => {
                     // Re-enable the form
                     this.signInForm.enable();
 
@@ -118,7 +115,7 @@ export class AuthSignInComponent implements OnInit
 
                     // Show the alert
                     this.showAlert = true;
-                },
-            );
+                }
+            });
     }
 }
