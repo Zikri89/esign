e53fe23fc0bea439db474d0e6f47a1224183f504
zikri89 <asalngoding89@gmail.com>
2023-12-09 10:37:20 +0700

    form

--- src/app/modules/admin/apps/file-manager/wizards/wizards.component.ts
+++ src/app/modules/admin/apps/file-manager/wizards/wizards.component.ts
@@ -1,15 +1,17 @@
 import { TextFieldModule } from '@angular/cdk/text-field';
-import { NgClass } from '@angular/common';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatOptionModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormManagerService } from 'app/modules/admin/master/form-manager/form-manager.service';
@@ -33,24 +35,30 @@ import { FormManagerData } from 'app/modules/admin/master/form-manager/form-mana
         MatSelectModule,
         MatOptionModule,
         MatChipsModule,
-        MatDatepickerModule
+        MatDatepickerModule,
+        CommonModule,
+        MatCheckboxModule,
+        MatRadioModule
 
     ]
 })
 export class FormsWizardsComponent implements OnInit
 {
+    @Input() formData: any[];
+    form: FormGroup;
+
     formFieldHelpers: string[] = [''];
     fixedSubscriptInput: FormControl = new FormControl('', [Validators.required]);
     dynamicSubscriptInput: FormControl = new FormControl('', [Validators.required]);
     fixedSubscriptInputWithHint: FormControl = new FormControl('', [Validators.required]);
     dynamicSubscriptInputWithHint: FormControl = new FormControl('', [Validators.required]);
-    esignData: FormManagerData;
     /**
      * Constructor
      */
     constructor(
         private _route: ActivatedRoute,
-        private _formManagerService : FormManagerService
+        private _formManagerService : FormManagerService,
+        private fb: FormBuilder
         )
     {
     }
@@ -64,21 +72,101 @@ export class FormsWizardsComponent implements OnInit
      */
     ngOnInit(): void
     {
+        this.formData = [
+            {
+              "createdAt": 1702046686664,
+              "updatedAt": 1702046686664,
+              "id": "65733c93b8389a274c222563",
+              "formName": "Rekam Medis Rawat Jalan",
+              "description": "memiliki fungsi untuk merekam terjadinya transaksi pelayanan yang dilakukan di unit rawat jalan. Formulir ini mencakup : Identitas pasien Anamnesa/pemeriksaan fisik Diagnosa Terapi yang diberikan Nama dan tanda tangan dokter Rekam asuhan keperawatan Formulir hasil-hasil penunjang medik Copy resep",
+              "formFields": [
+                {
+                  "type": "textarea",
+                  "label": "Alamat"
+                },
+                {
+                  "type": "text",
+                  "label": "Nama"
+                },
+                {
+                  "type": "date",
+                  "label": "Tanggal Lahir"
+                },
+                {
+                  "type": "select",
+                  "label": "Pasien",
+                  "options": [
+                    "Baru",
+                    "Lama"
+                  ]
+                },
+                {
+                  "type": "checkbox",
+                  "label": "Rawatan",
+                  "options": [
+                    "Rawat 1",
+                    "Rawat2"
+                  ]
+                },
+                {
+                    "type": "radio",
+                    "label": "Jenis Kelamin",
+                    "options": [
+                      "L",
+                      "P"
+                    ]
+                },
+                {
+                    "type": "radio",
+                    "label": "Status",
+                    "options": [
+                      "Suami",
+                      "Istri",
+                      "Anak",
+                    ]
+                },
+                {
+                    "type": "email",
+                    "label": "Email",
+                }
+              ]
+            }
+          ];
+
+        this.form = this.createForm();
+
         this._route.params.subscribe((params: Params) => {
             const formId = params['formulirId'];
 
-            this._formManagerService.onGetById(formId).subscribe({
-                next: (value) => {
-                    this.esignData = value;
-                }, error: (err) => {
-                    console.log(err);
-                }
-            });
+            // this._formManagerService.onGetById(formId).subscribe({
+            //     next: (value) => {
+            //         this.esignData = value;
+            //     }, error: (err) => {
+            //         console.log(err);
+            //     }
+            // });
         })
     }
 
-    getFormFieldHelpersAsString(): string
-    {
-        return this.formFieldHelpers.join(' ');
+    createForm() {
+        const formGroup = {};
+        this.formData[0].formFields.forEach((field) => {
+          if (field.type === 'checkbox') {
+            field.options.forEach((option) => {
+              formGroup[`${field.label}_${option}`] = this.fb.control(false);
+            });
+          } else {
+            formGroup[field.label] = this.fb.control('');
+          }
+        });
+        return this.fb.group(formGroup);
+      }
+
+      getFormControl(fieldName: string) {
+        return this.form.get(fieldName);
+      }
+
+    onSubmit() {
+        console.log(this.form.value);
     }
 }
